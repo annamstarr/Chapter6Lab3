8c8c64a498819ea406aa7f33f4f915d421887df5
Starr <2006183@student.acalanes.k12.ca.us>
2018-01-03 10:03:34 -0800

    holla

---
+++ Ch6Lab3/Example.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Example
+{
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList();
+        ArrayList<String> arr2 = new ArrayList<String>();
+        arr2.add("BE");
+        arr2.add("See you later");
+        
+        arr.add(1);
+        arr.add("HI");
+        System.out.println(arr);
+    }
+        
+}
